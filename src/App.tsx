aa33c0156cf2f106c0479a6e1769981e18e638ad
Randy Soriani <randysoriani@outlook.com>
2022-03-21 12:47:00 -0300

    WIP - new line animated too

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,7 @@
 import styled from 'styled-components';
-import {GlobalStyle} from './styles/global';
-
+import { useState, useRef } from 'react';
 import { motion, AnimatePresence } from 'framer-motion';
-import { useState } from 'react';
+import {GlobalStyle} from './styles/global';
 
 const Container = styled.div`
   width: 100vw;
@@ -42,12 +41,26 @@ const Container = styled.div`
       opacity: 0;
     }
 
+    .toggle-button{
+      background: none;
+      border: none;
+      color: white;        
+    }
+
   }
 `
 
 
 function App() {
-  const [isOpen, setIsOpen] = useState(false);
+  const [isLinesOpen, setIsLinesOpen] = useState(false);
+  const [isNewLineOpen, setIsNewLineOpen] = useState(false);
+  const [newLineTotal, setNewLineTotal] = useState(0);
+  const newLineQtyField = useRef<HTMLInputElement>(null);
+  const newLineUnitPriceField = useRef<HTMLInputElement>(null);
+
+  function calcLineTotal(){
+    setNewLineTotal(Number(newLineQtyField?.current?.value) * Number(newLineUnitPriceField?.current?.value))
+  }
 
   return (
     <>
@@ -66,7 +79,10 @@ function App() {
             <article className="line">
             
               <div className="summary">
-                <button onClick={()=>{setIsOpen(!isOpen)}}>v</button>
+                <motion.button className='toggle-button' onClick={()=>{setIsLinesOpen(!isLinesOpen)}}  initial="closed"
+                  animate={isLinesOpen ? "opened" : "closed"}
+                  variants={{ "opened": { rotate: '180deg' }, "closed": { rotate: '0deg'} }} >▼
+                </motion.button>
                 <p>6545654</p>
                 <p>2352,00</p>
                 <p>21/01/2021</p>
@@ -75,7 +91,7 @@ function App() {
               </div>
               
               <AnimatePresence initial={false}>
-              {isOpen && (
+              {isLinesOpen && (
                 <motion.div className="items" 
                 key="content"
                 initial="collapsed"
@@ -92,7 +108,7 @@ function App() {
                     <p>Qty</p>
                     <p>Unit price</p>
                     <p>Total</p>
-                    <button>+ Add line</button>
+                    <button onClick={()=> {setIsNewLineOpen(true)}}>+ Add line</button>
                   </div>
                   <ul>
                     <li className='detail-item'>
@@ -102,6 +118,30 @@ function App() {
                       <p>108.24</p>
                     </li>
                   </ul>
+                  <AnimatePresence initial={false}>
+                    {isNewLineOpen ? (
+                      <motion.div className="new-line" key="content"
+                      initial="collapsed"
+                      animate="open"
+                      exit="collapsed"
+                      variants={{
+                        open: { opacity: 1, height: "auto" },
+                        collapsed: { opacity: 0, height: 0 }
+                      }}
+                      transition={{ duration: 0.2 }}
+                      >
+                      <input type="text" placeholder='Ticker'  />
+                      <input type="text" placeholder='Quantity' onChange={calcLineTotal} ref={newLineQtyField} />
+                      <input type="text" placeholder='Unit price' onChange={calcLineTotal} ref={newLineUnitPriceField} />
+                      <input type="text" placeholder='Total' value={newLineTotal} readOnly={true} />
+                      <button onClick={()=>{setIsNewLineOpen(false); setNewLineTotal(0)}}>cancel</button>
+                      <button>save</button>
+                    </motion.div>
+                    ): (
+                      null
+                    )}
+                  </AnimatePresence>
+                    
                 </motion.div>
                 )}
                 </AnimatePresence>
@@ -171,8 +211,6 @@ function App() {
                 </div>
               
             </article>
-
-
 
           </div>
         </div>
